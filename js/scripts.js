8179ae68f1727bc11272c05bc3c0e3391fa61121
vova <pidvisennyjvolodimir@gmail.com>
2021-09-23 13:03:05 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function openLabItem(labItem) {
+
+    var i, labItemContent, labItemLinks;
+
+    labItemContent = document.getElementsByClassName("labItemContent");
+    for (i = 0; i < labItemContent.length; i++) {
+        labItemContent[i].style.display = "none";
+    }
+
+    document.getElementById(labItem).style.display = "block";
+
+}
+
+
+function openLab(labName) {
+
+    var i, labContent, labLinks;
+
+    labContent = document.getElementsByClassName("labContent");
+    for (i = 0; i < labContent.length; i++) {
+        labContent[i].style.display = "none";
+    }
+
+    document.getElementById(labName).style.display = "block";
+}
